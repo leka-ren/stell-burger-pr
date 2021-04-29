fd585f744c6f0c5ad02e2f3b0b111f9b1105e7ca
leka-ren <polafort@gmail.com>
2021-04-29 17:22:58 +0300

    костыль для стилей лого, очень нету времени делать нормально

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -13,18 +13,26 @@ function AppHeader() {
         <ul className="app-header__elements">
           <li className="app-header__element">
             <BurgerIcon type="primary" />
-            <p className="app-header__elemnt-text text_type_main-default">Конструктор</p>
+            <p className="app-header__elemnt-text text_type_main-default">
+              Конструктор
+            </p>
           </li>
           <li className="app-header__element">
             <ListIcon type="secondary" />
-            <p className="app-header__elemnt-text text_type_main-default">Лента Заказов</p>
+            <p className="app-header__elemnt-text text_type_main-default">
+              Лента Заказов
+            </p>
           </li>
         </ul>
       </nav>
-      <Logo />
+      <span style={{ marginLeft: "-135px" }}>
+        <Logo />
+      </span>
       <div className="app-header__element">
         <ProfileIcon type="secondary" />
-        <p className="app-header__elemnt-text text_type_main-default">Личный кабинет</p>
+        <p className="app-header__elemnt-text text_type_main-default">
+          Личный кабинет
+        </p>
       </div>
     </header>
   );
