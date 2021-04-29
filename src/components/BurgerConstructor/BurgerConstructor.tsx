70c6b14513cc14485297e9e8299a886e08d0b7e6
leka-ren <polafort@gmail.com>
2021-04-29 17:15:31 +0300

    доработаны стили и jsx для компонента BurgerConstructor

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -3,51 +3,89 @@ import burgerData from "../../const/burgerData";
 import {
   CurrencyIcon,
   LockIcon,
-  DragIcon
+  DragIcon,
+  Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import TrashIcon from "../../images/TrashIcon.svg";
 
 function BurgerConstructor() {
-  const BurgerMainItem = ({ data, blocked }: any) => {
+  const totalPrice = burgerData.reduce(
+    (acc, el) => (el.type === "main" ? acc + el.price : acc),
+    0
+  );
+  const BurgerMainItem = ({ data, blocked, first }: any) => {
     const styleItem = {
-      borderRadius: blocked ? "88px 88px 40px 40px" : "40px",
+      borderRadius: blocked
+        ? first
+          ? "88px 88px 40px 40px"
+          : "40px 40px 88px 88px"
+        : "40px",
     };
+
     return (
       <div style={styleItem} className="burger-constructor__item">
-        {!blocked && <DragIcon type="primary" />}
-        <img src={data.image_mobile} alt="картинка ингредиента" />
-        <p>{data.name}</p>
-        <div>
-          <p>{data.price}</p>
-          <CurrencyIcon type="primary" />
+        {!blocked && (
+          <span className="burger-constructor__item-dd-btn">
+            <DragIcon type="primary" />
+          </span>
+        )}
+        <img
+          className="burger-constructor__item-img"
+          src={data.image_mobile}
+          alt="картинка ингредиента"
+        />
+        <p className="burger-constructor__item-name">{data.name}</p>
+        <div className="burger-constructor__item-options">
+          <p className="text_type_digits-default burger-constructor__item-price">
+            {data.price}
+          </p>
+          <span className="burger-constructor__item-icon-price">
+            <CurrencyIcon type="primary" />
+          </span>
           {blocked ? (
-            <LockIcon type="secondary" />
+            <span className="burger-constructor__item-icon-lock">
+              <LockIcon type="secondary" />
+            </span>
           ) : (
-            <img src={TrashIcon} alt="иконка для удаления элемента" />
+            <span className="burger-constructor__item-icon-trash">
+              <img src={TrashIcon} alt="иконка для удаления элемента" />
+            </span>
           )}
         </div>
       </div>
     );
   };
   const bunData = burgerData.find((el) => el.type === "bun");
-  console.log(bunData);
+  const totalPriceWithBun = bunData?.price || 0;
   return (
     <div className="burger-constructor">
       <div className="burger-constructor__items">
-        <BurgerMainItem data={bunData} blocked={true} />
+        <BurgerMainItem data={bunData} blocked={true} first={true} />
         <div className="burger-constructor__items-main">
           {burgerData.map(
             (el) =>
               el.type === "main" && (
-                <BurgerMainItem key={el._id} data={el} blocked={false} />
+                <BurgerMainItem
+                  key={el._id}
+                  data={el}
+                  blocked={false}
+                  first={false}
+                />
               )
           )}
         </div>
-        <BurgerMainItem data={bunData} blocked={true} />
+        <BurgerMainItem data={bunData} blocked={true} first={false} />
       </div>
       <div className="burger-constructor__total">
-        <p className="burger-constructor__toral-price"></p>
-        <button></button>
+        <span className="burger-constructor__toral-price-content">
+          <p className="text_type_digits-default burger-constructor__toral-price">
+            {totalPrice + totalPriceWithBun}
+          </p>
+          <CurrencyIcon type="primary" />
+        </span>
+        <Button type="primary" size="medium">
+          Оформить заказ
+        </Button>
       </div>
     </div>
   );
--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -14,7 +14,7 @@ function BurgerIngredients() {
       <div className="burger-ingredients__item">
         <img src={props.image} alt="картинка" />
         <div className="burger-ingredients__price-content">
-          <p className="burger-ingredients__price">{props.price}</p>
+          <p className="text_type_digits-default burger-ingredients__price">{props.price}</p>
           <CurrencyIcon type="primary" />
         </div>
         <p>{props.name}</p>
@@ -35,16 +35,23 @@ function BurgerIngredients() {
           Three
         </Tab>
       </div>
-      <div className="burger-ingredients__items-content">
-        <p className="text_type_main-medium">Булки</p>
-        <div className="burger-ingredients__items">
-          {burgerData.map((el) => el.type === "bun" && <ItemMenu key={el._id} props={el} />)}
+      <div className="burger-ingredients__content">
+        <div className="burger-ingredients__items-content">
+          <p className="text_type_main-medium">Булки</p>
+          <div className="burger-ingredients__items">
+            {burgerData.map(
+              (el) => el.type === "bun" && <ItemMenu key={el._id} props={el} />
+            )}
+          </div>
         </div>
-      </div>
-      <div className="burger-ingredients__items-content">
-        <p className="text_type_main-medium">Соусы</p>
-        <div className="burger-ingredients__items">
-          {burgerData.map((el) => el.type === "sauce" && <ItemMenu key={el._id} props={el} />)}
+        <div className="burger-ingredients__items-content">
+          <p className="text_type_main-medium">Соусы</p>
+          <div className="burger-ingredients__items">
+            {burgerData.map(
+              (el) =>
+                el.type === "sauce" && <ItemMenu key={el._id} props={el} />
+            )}
+          </div>
         </div>
       </div>
     </div>
