431ad04e9fd9d442f10f4dab7101a9f768c85096
leka-ren <polafort@gmail.com>
2021-05-06 17:20:15 +0300

    реализация рендера модального окна через портал

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,18 +1,18 @@
 import stylesModal from "./Modal.module.css";
 import PropTypes from "prop-types";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import ReactDOM from "react-dom";
 
 function Modal({ showModal, children }: any) {
-  return (
+  const modalRoot:any = document.querySelector("#modal-container");
+  return ReactDOM.createPortal(
     <div className={stylesModal.modal__window}>
-      <span
-        className={stylesModal.modal__closeIcon}
-      >
+      <span className={stylesModal.modal__closeIcon}>
         <CloseIcon onClick={() => showModal(false)} type="primary" />
       </span>
       {children}
     </div>
-  );
+  , modalRoot);
 }
 
 Modal.propTypes = {
